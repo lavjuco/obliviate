2967c90937de2a43fe5edb86e88c6c2f57dcc6cc
lavjuco <139962025+lavjuco@users.noreply.github.com>
2024-01-02 15:49:44 +0200

    refactoring

--- obliviate.go
+++ obliviate.go
@@ -1,86 +1,128 @@
 package main
 
-import ( 
-	"net/http"
-	"io"
+import (
 	"fmt"
-	"encoding/json"
-	"time"
-	"strconv"
 	"os"
+	"encoding/json"
+	"strconv"
+	"net/http"
+	"io/ioutil"
+	"time"
 )
 
-type Message struct {
-	Id string
-	Content string 
-	Channel string `json:"channel_id"`
-}
-
-type Search struct {
-	TotalResults int `json:"total_results"`
-	Messages [][]Message
-}
-
-type Preset struct {
-	Token string
-	Author string
-	Channel []string
-}
-
-var p Preset
-
-func actionRequest(method string, url string) ([]byte, int) {
-	req, _ := http.NewRequest(method, url, nil)
-	req.Header.Add("Authorization", p.Token)
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-	body, _ := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-
-	return body, resp.StatusCode
-}
-
-func getMessages(offset int, channel string) Search {
-	body, statusCode := actionRequest("GET", "https://discord.com/api/v9/channels/"+channel+"/messages/search?author_id="+p.Author+"&offset="+strconv.Itoa(offset))
-
-	if statusCode == 404 {
-		body, statusCode = actionRequest("GET", "https://discord.com/api/v9/guilds/"+channel+"/messages/search?author_id="+p.Author+"&offset="+strconv.Itoa(offset))
+func main() {
+	type Credentials struct {
+		Token, Author string
+		Channels []string
 	}
 
-	var s Search
-	json.Unmarshal(body, &s)
+	type Message struct {
+		Id string
+		Type int
+		Channel string `json:"channel_id"`
+	}
 
-	return s
-}
+	type Messages struct {
+		Total int `json:"total_results"`
+		Messages [][]Message
+	}
 
-func main() {
-	preset, _ := os.ReadFile("preset.json")
-	json.Unmarshal(preset, &p)
+	stack := map[string][]string{}
 
-	for _, channel := range p.Channel {
+	p, err := os.ReadFile("preset.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-		var offset int = 0
-		var totalResults int = 9999999
+	var preset Credentials 
+	err = json.Unmarshal(p, &preset)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-		for totalResults != offset {
-			s := getMessages(offset, channel)
-			totalResults = s.TotalResults
+	client := &http.Client{}
 
-			if offset == totalResults {
-				break
+	for _, channel := range preset.Channels {
+		offset := 0
+		totalMessages := 1
+		for offset < totalMessages {
+			req, err := http.NewRequest("GET", 
+			"https://discord.com/api/v9/channels/"+channel+
+			"/messages/search?author_id="+preset.Author+
+			"&offset="+strconv.Itoa(offset), 
+			nil)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			req.Header.Add("Authorization", preset.Token)
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				return
 			}
 
-			for _, msg := range s.Messages {
-				_, statusCode := actionRequest("DELETE", "https://discord.com/api/v9/channels/"+msg[0].Channel+"/messages/"+msg[0].Id)
 
-				if statusCode == 403 {
-					offset++
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var messages Messages
+			err = json.Unmarshal(body, &messages)
+
+			if resp.StatusCode == 200 {
+				offset += len(messages.Messages) 
+				totalMessages = messages.Total
+			}
+
+			fmt.Println("fetching messages "+
+			strconv.Itoa(offset)+"/"+strconv.Itoa(totalMessages)+
+			" from channel "+channel, resp.StatusCode)
+
+			for _, message := range messages.Messages {
+				if message[0].Type == 0 {
+					stack[message[0].Channel] = append(
+						stack[message[0].Channel], 
+						message[0].Id)
 				}
-
-				time.Sleep(3 * time.Second)
-
-				fmt.Println(msg[0].Id, statusCode)
 			}
+
+			resp.Body.Close()
+			time.Sleep(5 * time.Second)
 		}
 	}
+
+	i := 0
+	for channel, messages := range stack {
+		t := 0
+		for t < len(messages) {
+			req, err := http.NewRequest("DELETE", 
+			"https://discord.com/api/v9/channels/"+channel+
+			"/messages/"+messages[t], nil)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			req.Header.Add("Authorization", preset.Token)
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("deleting message "+strconv.Itoa(t+1)+"/"+strconv.Itoa(len(messages))+" "+messages[t]+
+			" from channel "+strconv.Itoa(i+1)+"/"+strconv.Itoa(len(stack))+" "+channel, resp.StatusCode)
+			if resp.StatusCode == 204 {
+				t += 1
+			}
+			resp.Body.Close()
+			time.Sleep(3 * time.Second)
+		}
+
+		i += i
+	}
+	return
 }
