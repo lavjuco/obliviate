fa1d6a21234123bb3dbf09d2f0b4d6ab002f16ae
lavjuco <139962025+lavjuco@users.noreply.github.com>
2024-01-02 20:01:15 +0200

    guilds support

--- obliviate.go
+++ obliviate.go
@@ -4,29 +4,29 @@ import (
 	"fmt"
 	"os"
 	"encoding/json"
-	"strconv"
 	"net/http"
-	"io/ioutil"
 	"time"
+	"io/ioutil"
+	"strconv"
 )
 
+type Credentials struct {
+	Token, Author string
+	Channels []string
+}
+
+type Message struct {
+	Id string
+	Type int
+	Channel string `json:"channel_id"`
+}
+
+type Messages struct {
+	Total int `json:"total_results"`
+	Messages [][]Message
+}
+
 func main() {
-	type Credentials struct {
-		Token, Author string
-		Channels []string
-	}
-
-	type Message struct {
-		Id string
-		Type int
-		Channel string `json:"channel_id"`
-	}
-
-	type Messages struct {
-		Total int `json:"total_results"`
-		Messages [][]Message
-	}
-
 	stack := map[string][]string{}
 
 	p, err := os.ReadFile("preset.json")
@@ -45,14 +45,16 @@ func main() {
 	client := &http.Client{}
 
 	for _, channel := range preset.Channels {
+		target := "channels"	
 		offset := 0
 		totalMessages := 1
 		for offset < totalMessages {
 			req, err := http.NewRequest("GET", 
-			"https://discord.com/api/v9/channels/"+channel+
+			"https://discord.com/api/v9/"+target+"/"+channel+
 			"/messages/search?author_id="+preset.Author+
 			"&offset="+strconv.Itoa(offset), 
 			nil)
+
 			if err != nil {
 				fmt.Println(err)
 				return
@@ -64,6 +66,16 @@ func main() {
 				return
 			}
 
+			if resp.StatusCode == 404 {
+				fmt.Println("not found in", target)
+				if target == "channels" {
+					target = "guilds"
+				} else {
+					target = "channels"
+				}
+				fmt.Println("look for", target)
+				continue
+			}
 
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
@@ -77,11 +89,12 @@ func main() {
 			if resp.StatusCode == 200 {
 				offset += len(messages.Messages) 
 				totalMessages = messages.Total
+			} else {
+				fmt.Println("not expected response", resp.StatusCode, "try again")
+				continue
 			}
 
-			fmt.Println("fetching messages "+
-			strconv.Itoa(offset)+"/"+strconv.Itoa(totalMessages)+
-			" from channel "+channel, resp.StatusCode)
+			fmt.Println("fetching messages", offset, "/", totalMessages, "from channel", channel, resp.StatusCode)
 
 			for _, message := range messages.Messages {
 				if message[0].Type == 0 {
@@ -113,8 +126,7 @@ func main() {
 				fmt.Println(err)
 				return
 			}
-			fmt.Println("deleting message "+strconv.Itoa(t+1)+"/"+strconv.Itoa(len(messages))+" "+messages[t]+
-			" from channel "+strconv.Itoa(i+1)+"/"+strconv.Itoa(len(stack))+" "+channel, resp.StatusCode)
+			fmt.Println("deleting message ", (t+1), "/", len(messages), messages[t], "from channel", (i+1), "/", len(stack), channel, resp.StatusCode)
 			if resp.StatusCode == 204 {
 				t += 1
 			}
@@ -122,7 +134,7 @@ func main() {
 			time.Sleep(3 * time.Second)
 		}
 
-		i += i
+		i += 1
 	}
 	return
 }
